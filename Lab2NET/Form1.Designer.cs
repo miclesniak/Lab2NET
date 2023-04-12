4de7b6fff0f307dd878904fd4962d54a46907140
SkibA4 <jakub.skibinski16@gmail.com>
2023-04-12 16:24:36 +0200

    Dokończenie funkcjionalności pobierania z API.

--- Lab2NET/Form1.Designer.cs
+++ Lab2NET/Form1.Designer.cs
@@ -28,84 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1=new Button();
-            label1=new Label();
-            pictureBox1=new PictureBox();
-            textBox1=new TextBox();
-            textBox2=new TextBox();
-            textBox3=new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location=new Point(414, 376);
-            button1.Name="button1";
-            button1.Size=new Size(106, 46);
-            button1.TabIndex=1;
-            button1.Text="button1";
-            button1.UseVisualStyleBackColor=true;
-            button1.Click+=button1_Click;
+            button1.Location = new Point(414, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 46);
+            button1.TabIndex = 1;
+            button1.Text = "Wyszukaj piwo!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
-            label1.AutoSize=true;
-            label1.Font=new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location=new Point(376, 41);
-            label1.Name="label1";
-            label1.Size=new Size(68, 30);
-            label1.TabIndex=3;
-            label1.Text="label1";
-            label1.Click+=label1_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(376, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 30);
+            label1.TabIndex = 3;
+            label1.Click += label1_Click;
             // 
             // pictureBox1
             // 
-            pictureBox1.Location=new Point(32, 87);
-            pictureBox1.Name="pictureBox1";
-            pictureBox1.Size=new Size(206, 257);
-            pictureBox1.TabIndex=4;
-            pictureBox1.TabStop=false;
-            pictureBox1.Click+=pictureBox1_Click;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(32, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(206, 381);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // textBox1
             // 
-            textBox1.Location=new Point(259, 389);
-            textBox1.Name="textBox1";
-            textBox1.Size=new Size(100, 23);
-            textBox1.TabIndex=5;
-            textBox1.TextChanged+=textBox1_TextChanged;
+            textBox1.Location = new Point(283, 389);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location=new Point(283, 87);
-            textBox2.Multiline=true;
-            textBox2.Name="textBox2";
-            textBox2.Size=new Size(461, 115);
-            textBox2.TabIndex=6;
-            textBox2.TextChanged+=textBox2_TextChanged;
+            textBox2.Location = new Point(283, 87);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(461, 115);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location=new Point(283, 223);
-            textBox3.Multiline=true;
-            textBox3.Name="textBox3";
-            textBox3.Size=new Size(461, 121);
-            textBox3.TabIndex=7;
+            textBox3.Location = new Point(283, 223);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(461, 121);
+            textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(283, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Opis";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(283, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Wskazówki";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(283, 371);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Podaj id";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(32, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Zdjęcie";
             // 
             // Form1
             // 
-            AutoScaleDimensions=new SizeF(7F, 15F);
-            AutoScaleMode=AutoScaleMode.Font;
-            ClientSize=new Size(800, 450);
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
             Controls.Add(button1);
-            Name="Form1";
-            Text="Form1";
-            Load+=Form1_Load;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -118,5 +163,9 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
